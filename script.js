56e51430c84eb4fe66e7f7d20b51535f5fa8cfe9
Jan Erik Winterer <janerik.winterer@gmail.com>
2023-11-27 10:31:08 +0100

    updated csv fetching

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
-var fs = require('fs');
-var csv = require('csv-parser')
-
-const csvFilePath = ".\hangman_words.csv"
+const csvUrlPath = "https://github.com/JanWinterer/Hangman/blob/9e3f49e58445cd61fd0a7361e2bc3dcaffb078c3/hangman_words.csv"
 
 function onButtonClick() {
-    const hangmanStr = getRandomElementFromCSV(csvFilePath)
+    var hangmanStr = getRandomElementFromCSV(csvUrlPath)
     document.getElementById("changed").textContent = "Your Hangman Word is:" + hangmanStr;
+}
+
+async function fetchCSV(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.text();
+}
+
+function parseCSV(csvData) {
+    const lines = csvData.split("\n");
+    const result = [];
+    const headers = lines[0].split(",");
+
+    for (let i = 1; i < lines.length; i++) {
+        let obj = {};
+        let currentline = lines[i].split(",");
+
+        for (let j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentline[j];
+        }
+
+        result.push(obj);
+    }
+
+    return result; // This is an array of objects
 }
 
 document.getElementById("Button1").addEventListener("click", onButtonClick);
 
-function getRandomElementFromCSV(filePath) {
-    return new Promise((resolve, reject) => {
-        const values = [];
 
-        fs.createReadStream(filePath)
-        .pipe(csv())
-        .on('data', (row) => {
-            const value = row[Object.keys(row)[0]];
-            values.push(value);
-        })
-        .on('end', () => {
-            if (values.lenght == 0) {
-                reject(new Error("CSV is empty"));
-            } else {
-                const randomIndex = Math.floor(Math.random() * values.length);
-                const randomValue = values[randomIndex];
-                resolve(randomValue);
+function getRandomElementFromCSV(csvUrlPath) {
+    return fetchCSV(csvUrlPath)
+        .then(csvData => {
+            const parsedCSV = parseCSV(csvData);
+            if (parseCSV.length === 0){
+                throw new Error("CSV is empty");
             }
-        })
-        .on('error', (error) => {
-            reject(new Error('failed to open CSV file: ${randomValue}'));
-        })
-        .catch((error) => {
-            console.error(error.message);
-        })
+            const randomIndex = Math.floor((Math.random() * parsedCSV.length()));
+            const randomValue = parsedCSV[randomIndex];
+            return randomValue;
     })
+    .catch(error => {
+        console.error("Error feching CSV data!");
+        throw error;
+    });
 }
