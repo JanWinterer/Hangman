ffa86c8558758cf665ead18079876c0202c2858a
Jan Erik Winterer <janerik.winterer@gmail.com>
2023-11-27 10:45:04 +0100

    1

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const csvUrlPath = "https://raw.githubusercontent.com/JanWinterer/Hangman/main/hangman_words.csv"
 
-function onButtonClick() {
-    var hangmanStr = getRandomElementFromCSV(csvUrlPath)
-    document.getElementById("changed").textContent = "Your Hangman Word is:" + hangmanStr;
+async function onButtonClick() {
+    try {
+        var hangmanStr = await getRandomElementFromCSV(csvUrlPath);
+        document.getElementById("changed").textContent = "Your Hangman Word is: " + hangmanStr;
+    } catch (error) {
+        console.error("Error fetching or processing the CSV data: ", error);
+        document.getElementById("changed").textContent = "Error fetching word.";
+    }
 }
+
 
 async function fetchCSV(url) {
     const response = await fetch(url);
